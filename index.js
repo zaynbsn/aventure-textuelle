07a20b483655f36a3cb350dff327914a72c8a6c8
zaynbsn <nils.vainui@gmail.com>
2022-10-11 20:14:17 +0200

    add chalk lib to have colors in terminal

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log('hello world')
+import chalk from 'chalk'
+
+console.log(chalk.red('red'))
+console.log(chalk.yellow('yellow'))
+console.log(chalk.green('green'))
+console.log(chalk.bold('bold'))
+console.log(chalk.red.bold('red & bold'))
+console.log(chalk.bgMagenta('magenta bg'))
